063b76ee4efb4801956acfd4e70df5a76162a905
HariBhavis <85010458+HariBhavis@users.noreply.github.com>
2021-05-29 14:57:54 +0530

    Create 2D-Array-DS.js

---
+++ 2D-Array-DS.js
@@ -0,0 +1,33 @@
+
+function hourglassSum(arr) {
+    let maxValue;
+    let outerCounter = 0;
+    for (let i = 0; i < 6; i++) {
+        outerCounter++;
+        if (outerCounter > 4) {
+            outerCounter = 0;
+            break;
+        }
+        let counter = 0;
+        for (let j = 0; j < 6; j++) {
+            counter++;
+            if (counter > 4) {
+                counter = 0;
+                break;
+            }
+
+            let value = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] +
+                arr[i + 1][j + 1] +
+                arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+
+            if (maxValue === undefined) {
+                maxValue = value
+            } else if (value > maxValue) {
+                maxValue = value;
+            }
+        }
+    }
+
+    return maxValue;
+
+}
